b2825ce576b7664b9df03579a3f59400975d8882
maxgunchoi <chleorjs37@gmail.com>
2022-05-26 08:46:24 +0900

    Rxjs mergeMap 진행중

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MergemapComponent } from './pages/mergemap/mergemap.component';
+import { ConcatmapComponent } from './pages/concatmap/concatmap.component';
+import { SwitchmapComponent } from './pages/switchmap/switchmap.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MergemapComponent,
+    ConcatmapComponent,
+    SwitchmapComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/pages/mergemap/mergemap.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { concatMap, delay, fromEvent, mergeMap, of, tap } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
+
+const API_URL = 'https://jsonplaceholder.typicode.com/todos/1';
+@Component({
+  selector: 'app-mergemap',
+  templateUrl: './mergemap.component.html',
+  styleUrls: ['./mergemap.component.scss']
+})
+export class MergemapComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+    this.click();
+
+    //ajaxMergeMap
+    this.ajaxMergeMap();
+  }
+
+  saveLocation(location:any){
+    return of(location).pipe(delay(2000));
+  }
+
+  click(){
+    const click$ = fromEvent(document,'click');
+
+    click$.pipe(
+      mergeMap((e:any) =>{
+        return this.saveLocation({
+          x: e.clientX,
+          y: e.clientY,
+          timestamp: Date.now()
+        });
+      })
+    ).subscribe(r => console.log('Saved : ',r));
+  }
+
+  ajaxMergeMap(){
+    const btn1 = document.getElementById('btn1') as HTMLElement;
+    const click$ = fromEvent(btn1,'click');
+
+    click$.pipe(
+      mergeMap(() => ajax.getJSON(API_URL))
+    )
+    .subscribe(console.log);
+  }
+
+}
